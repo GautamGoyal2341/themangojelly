a0bce98be3abbf7ebf244c151b7e607741b28a8e
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-12-06 11:23:07 +0530

    routing setup + cart empty(with material ui done)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Routes,Route} from "react-router-dom";
+import CardsDetails from './components/CardsDetails';
+import Cards from './components/Cards';
 import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-     <>
-      <Header/>
-     </>
-    </div>
+    <>
+   
+   <Header />
+   <Routes>
+     <Route path='/' element={<Cards />} />
+     <Route path='/cart' element={<CardsDetails />} />
+   </Routes>
+  </>
   );
 }
 
---
+++ src/components/CardsDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardsDetails = () => {
+  return (
+    <div>CardsDetail</div>
+  )
+}
+
+export default CardsDetails
